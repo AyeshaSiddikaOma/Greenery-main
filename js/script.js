2f05c5c48f716a49c4246534af47b674f55dc776
ayeshasiddikaoma-cmyk <ayeshasiddikaoma@gmail.com>
2026-09-25 00:30:30 +0600

    add

--- js/script.js
+++ js/script.js
@@ -1,14 +1,15 @@
-
-let  fruit = "pineapple"
+let fruit = "pineapple";
 
 switch (fruit) {
-    case  "apple":
-        console.log("It is red")
-        break;
-    case  "pineapple":
-        console.log("It is yellow")
-        break;
+  case "apple":
+    console.log("It is red");
+    break;
 
-    default:
-        break;
+  case "pineapple":
+    console.log("It is yellow");
+    break;
+
+  default:
+    console.log("Pleae Enter The Name Of Any Fruit")
+    break;
 }
