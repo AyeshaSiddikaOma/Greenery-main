e5a174397718a56d66f49c6d4f2ad1331741cde9
ayeshasiddikaoma-cmyk <ayeshasiddikaoma@gmail.com>
2026-09-25 00:56:08 +0600

    add

--- js/script.js
+++ js/script.js
@@ -1,22 +1,44 @@
 
 
-let input = document.querySelector('#input').value
+let input = document.querySelector('#input')
 let btn = document.querySelector('#btn')
-
+let head = document.querySelector('#heading')
 btn.addEventListener('click',()=>{
 
 
-switch (input) {
+switch (input.value) {
   case "apple":
-    console.log("It is red");
-    break;
+  head.innerHTML = `
+             🍎 Apple is a nutritious fruit.<br>  🍎
+            It contains fiber and antioxidants.<br>  🍎
+            It may support heart health and digestion.<br>  🍎
+            Apples are also a good source of vitamin C.  
+          `;    
+              head.style.color = "red";
 
-  case "pineapple":
-    console.log("It is yellow");
-    break;
+          break;
+          
+  case "mango":
+  head.innerHTML = `
+    🥭 Mango is a delicious and nutritious fruit.<br> 🥭
+    It is rich in vitamin C and vitamin A.<br> 🥭
+    It contains antioxidants and fiber.<br> 🥭
+    Mango may support healthy digestion.
+  `
+          break;
+
+  case "avocado":
+   head.innerHTML = `
+    🥑 Avocado is a nutritious fruit.<br> 🥑
+    It contains healthy fats and fiber.<br> 🥑
+    It is a good source of potassium and vitamins.<br> 🥑
+    Avocado may support heart health.
+  `;
+          break;
+
 
   default:
-    console.log("Pleae Enter The Name Of Any Fruit")
+    head.innerHTML = "Sorry! We don't have information about this fruit."
     break;
 }
 
